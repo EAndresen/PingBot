9b4bfa9ce9bf2cfcb78d8556ac3abe61314588e7
Eric Andresen <eric@andresen.codes>
2018-01-16 13:28:14 +0100

    Version 1.0B First commit.

---
+++ src/sample/Controller.java
@@ -0,0 +1,78 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    Button button = new Button();
+
+    @FXML
+    TextField ipInputTextField = new TextField();
+
+    @FXML
+    TextField countInputTextField = new TextField();
+
+    @FXML
+    TextField sizeInputTextField = new TextField();
+
+    @FXML
+    TextField ipEndInputTextField = new TextField();
+
+    @FXML
+    RadioButton infinityRadioButton = new RadioButton();
+
+    private Pinger pinger = new Pinger();
+
+    private String inputStartIP;
+    private int inputEndOfEndIP;
+    private int numberOfPings;
+    private int infinityPings;
+
+    @FXML
+    private void initialize() {
+        infinityRadioButton.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if(infinityRadioButton.isSelected()){
+                countInputTextField.setDisable(true);
+                infinityPings = -1;
+            }else{
+                countInputTextField.setDisable(false);
+                infinityPings = 0;
+            }
+        });
+
+        button.setOnAction(event -> {
+            inputStartIP = ipInputTextField.getText();
+            inputEndOfEndIP = Integer.parseInt(ipEndInputTextField.getText());
+
+            int pos = inputStartIP.lastIndexOf(".");
+            int endOfFirstIP = Integer.parseInt(inputStartIP.substring(pos+1, inputStartIP.length()));
+            String beginningOfFirstIP = inputStartIP.substring(0, pos);
+            numberOfPings = inputEndOfEndIP - endOfFirstIP +1;
+
+            for(int i = 0; i < numberOfPings; i++){
+                String ip = beginningOfFirstIP + "." + Integer.toString(endOfFirstIP);
+                String count;
+                String size = sizeInputTextField.getText();
+
+                if(infinityPings == -1){
+                    count = "-1";
+                }else {
+                    count = countInputTextField.getText();
+                }
+
+                try {
+                    pinger.ping(ip, count, size);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                endOfFirstIP++;
+            }
+        });
+    }
+}
---
+++ src/sample/Pinger.java
@@ -0,0 +1,69 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.*;
+
+class Pinger {
+    private Runtime r;
+    private Process p;
+
+    void ping(String ipInput, String countInput, String sizeInput) throws IOException {
+      TextArea textArea = new TextArea();
+      createPingWindow(textArea);
+
+      String pingCmd = createPingLine(countInput, sizeInput, ipInput);
+
+      Thread thread = createPingThread(pingCmd, textArea);
+      thread.start();
+    }
+
+    private Thread createPingThread(String pingCmd, TextArea textArea) {
+        return new Thread(() -> {
+            try {
+                r = Runtime.getRuntime();
+                p = r.exec(pingCmd);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    final String outputLine = inputLine;
+                    Platform.runLater(() -> textArea.appendText(outputLine + "\n"));
+                }
+                in.close();
+            } catch (IOException event) {
+                System.out.println(event);
+            }
+        });
+    }
+
+    private void killRuntimeProcess() {
+        p.destroy();
+    }
+
+    private void createPingWindow(TextArea textArea) {
+        VBox vBox = new VBox(textArea);
+        textArea.setPrefHeight(350);
+        textArea.setPrefWidth(350);
+        Stage stage = new Stage();
+        Scene scene = new Scene(vBox, 350, 350);
+        scene.getStylesheets().add(getClass().getResource("Stylesheet.css").toString());
+        stage.setScene(scene);
+        stage.setOnHidden(e -> killRuntimeProcess());
+        stage.show();
+    }
+
+    private String createPingLine(String countInput, String sizeInput, String ipInput) {
+        if (countInput.equals("-1")){
+            countInput = "-t ";
+        } else {
+            countInput = "-n " + countInput;
+        }
+
+        return "ping " + countInput + " -l " + sizeInput + " " + ipInput;
+    }
+}
